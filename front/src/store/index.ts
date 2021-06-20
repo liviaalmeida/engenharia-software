109a6e95c5cf7e82e1989cab7ecbdec3c90c0008
Lívia Almeida <leave.ah@gmail.com>
2021-06-20 11:19:01 -0300

    Added login and router guard

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    logged: false,
   },
   mutations: {
   },
   actions: {
+  },
+  getters: {
+    logged: (state) => state.logged,
   },
   modules: {
   }
