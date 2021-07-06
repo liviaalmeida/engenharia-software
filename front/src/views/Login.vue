236312061abe6aaa149382be11e72e9881726401
Lívia Almeida <leave.ah@gmail.com>
2021-07-06 17:50:16 -0300

    Started integration with api in front

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -2,18 +2,22 @@
   <div class="login">
     <h1>desapega</h1>
     <h2>doe. encontre. troque. ajude o planeta!</h2>
-    <form class="login-form" @submit.prevent>
+    <form class="login-form" @submit.prevent="onSubmit">
       <DsInput label="Email" type="email"
-      placeholder="Digite o email de login ou cadastro"
-      v-model="login.name" required />
+      placeholder="Digite seu email"
+      v-model="user.email" required />
       <DsInput label="Senha" type="password"
       placeholder="Digite a senha"
       :input-attrs="{ minLength: 8 }"
-      v-model="login.pass" required />
+      v-model="user.password" required />
       <DsButton>
-        Login
+        {{ button }}
       </DsButton>
     </form>
+    <button class="login-action"
+    @click="registeredUser = !registeredUser">
+      {{ action }}
+    </button>
   </div>
 </template>
 
@@ -23,11 +27,41 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      login: {
-        name: '',
-        pass: '',
+      user: {
+        email: '',
+        password: '',
       },
+      registeredUser: true,
     }
+  },
+  computed: {
+    action(): string {
+      return this.registeredUser ?
+        'Não tem conta? Clique aqui para fazer cadastro' :
+        'Já tem conta? Clique aqui para fazer login'
+    },
+    button(): string {
+      return this.registeredUser ?
+        'Login' :
+        'Signup'
+    },
+  },
+  methods: {
+    async onLogin() {
+      const response = await this.$api.post('/login', this.user)
+      console.log(response)
+    },
+    async onRegister() {
+      const response = await this.$api.post('/login/register', this.user)
+      console.log(response)
+    },
+    async onSubmit() {
+      if (this.registeredUser) {
+        await this.onLogin()
+      } else {
+        await this.onRegister()
+      }
+    },
   },
 })
 </script>
@@ -49,6 +83,14 @@ export default Vue.extend({
     margin-top: 25px;
     max-width: 350px;
     width: 100%;
+  }
+
+  &-action {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-family: 'Roboto';
+    margin-top: 20px;
   }
 }
 </style>
