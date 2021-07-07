b70c77cc5317daa4564e079a51228796be6c70c8
Lívia Almeida <leave.ah@gmail.com>
2021-07-06 21:39:48 -0300

    Added loading and improved login in store

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,19 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cookie from 'vue-cookies'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    logged: false,
+    loading: false,
+    // @ts-expect-error access cookie in server-side
+    logged: !!cookie.get('token'),
   },
   mutations: {
+    setLoading(state, payload: boolean) {
+      state.loading = payload
+    },
+    setLogged(state, payload: boolean) {
+      state.logged = payload
+    },
   },
   actions: {
+    startLoading({ commit }) {
+      commit('setLoading', true)
+    },
+    stopLoading({ commit }) {
+      commit('setLoading', false)
+    },
+    login({ commit }, payload: string) {
+      commit('setLogged', true)
+      // @ts-expect-error access cookie in server-side
+      cookie.set('token', payload)
+    },
+    logout({ commit }) {
+      commit('setLogged', false)
+      // @ts-expect-error access cookie in server-side
+      cookie.remove('token',)
+    },
   },
   getters: {
+    loading: (state) => state.loading,
     logged: (state) => state.logged,
   },
-  modules: {
-  }
 })
